9610a3a923abba6c305e36f6659d0370d300c1e0
Elise RECEJAC <elise.recejac@gmail.com>
2024-08-29 10:48:21 +0200

    exercise3

--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import Guess from "../Guess";
+import { range } from "../../utils";
 
 function GuessList({ guessList }) {
+  const getItemFromGuessList = (index) => {
+    return guessList[index]?.split("") ?? ["", "", "", "", ""];
+  };
   return (
     <div className="guess-results">
-      {guessList.map((guess, index) => (
-        <p className="guess" key={index}>
-          {guess}
-        </p>
+      {range(0, NUM_OF_GUESSES_ALLOWED).map((index) => (
+        <Guess guess={getItemFromGuessList(index)} key={index} />
       ))}
     </div>
   );
