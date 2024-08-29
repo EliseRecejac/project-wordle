2227c2a0accc9ab6a02fa71d5bbe1a87f9f2a76d
Elise RECEJAC <elise.recejac@gmail.com>
2024-08-29 15:38:03 +0200

    exercise5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,23 +5,67 @@ import { checkGuess } from "../../game-helpers";
 import { WORDS } from "../../data";
 import GuessInput from "../GuessInput";
 import GuessList from "../GuessList";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
+function HappyBanner({ numberOfGuesses }) {
+  return (
+    <div className="happy banner">
+      <p>
+        <strong>Congratulations!</strong> Got it in{" "}
+        <strong>{numberOfGuesses} guesses</strong>.
+      </p>
+    </div>
+  );
+}
+
+function SadBanner() {
+  return (
+    <div class="sad banner">
+      <p>
+        Sorry, the correct answer is <strong>{answer}</strong>.
+      </p>
+    </div>
+  );
+}
+
 function Game() {
   const [guessList, setGuessList] = useState([]);
+  const [status, setStatus] = useState("running");
+
+  const isGuessCorrect = (guess) => {
+    let isCorrect = true;
+    for (letter of guess) {
+      if (letter.status !== "correct") {
+        isCorrect = false;
+        break;
+      }
+    }
+    return isCorrect;
+  };
 
   const addNewGuess = (guess) => {
-    setGuessList([...guessList, checkGuess(guess, answer)]);
+    const nextGuess = checkGuess(guess, answer);
+    const nextGuessList = [...guessList, nextGuess];
+    setGuessList(nextGuessList);
+    console.log(guessList.length);
+    if (isGuessCorrect(nextGuess)) {
+      setStatus("won");
+    } else if (nextGuessList.length === NUM_OF_GUESSES_ALLOWED) {
+      setStatus("lost");
+    }
   };
 
   return (
     <>
       <GuessList guessList={guessList} />
-      <GuessInput addNewGuess={addNewGuess} />
+      {status === "running" && <GuessInput addNewGuess={addNewGuess} />}
+      {status === "won" && <HappyBanner numberOfGuesses={guessList.length} />}
+      {status === "lost" && <SadBanner />}
     </>
   );
 }
